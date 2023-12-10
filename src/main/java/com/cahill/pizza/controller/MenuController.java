7e1656264726b87e431c13d45222f0399d71b4b8
Codez4 Tacoz <6731652+codez4Tacoz@users.noreply.github.com>
2023-12-10 16:55:29 -0500

    Added more menu items, added menuItem component to display them, added some basic styling...

--- src/main/java/com/cahill/pizza/controller/MenuController.java
+++ src/main/java/com/cahill/pizza/controller/MenuController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.inject.Inject;
 import java.util.List;
 
 @RestController
